41026dd47626326626a6dab5faf39ccd01318f54
josephsbtn <132117803+josephsbtn@users.noreply.github.com>
2024-06-12 08:24:33 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat"],
+        lato: ["Lato"],
+        garamond: ["Garamond"],
+      },
+    },
+    colors: {
+      myBlue: "#5863F8",
+      myBlack: "#171D1C",
+      myWhite: "#FFFFFF",
+    },
+    backgroundImage: {
+      heroBg: "url('../img/image 9.png')",
+      catMen: "url('../img/cat-men.png')",
+      catWomen: "url('../img/cat-women.png')",
+      catKids: "url('../img/cat-kids.png')",
+    },
+  },
+  plugins: [],
+};
